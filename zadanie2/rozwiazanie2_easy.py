b625e84238b80cbd1546751d0db20bca5b13e081
Piotr Gdowski <piotrekgdowski@wp.pl>
2016-07-23 10:13:58 +0200

    Dorzucenie 10 spacji przed histogramem

--- zadanie2/rozwiazanie2_easy.py
+++ zadanie2/rozwiazanie2_easy.py
@@ -1,16 +1,12 @@
 #wyzwaniepython ZADANIE 2 (EASY)
 #27-07-2016
 #autor: @Dewastators
-import time
 import os.path
-import shutil
 
-def line(key, tup, count, extlen= 5, sizelen= 15, histlen=30):
+def line(key, tup, count, extlen= 5, sizelen= 15, histlen=50):
     size, many = tup
-    return key.rjust(extlen)+str(size).rjust(sizelen)+(int(round(histlen*many/count))*"#").rjust(histlen)
+    return "{}{}{}{}".format(key.rjust(extlen),str(size).rjust(sizelen),10*' ',(int(round(histlen*many/count))*"#").rjust(histlen))
 def info(way, data): # jestesmy w folderze way
-    # jezeli cos jest plikiem, to dorzucamy do slownika rozmiar
-    # jesli nie to tylko wchodzimy glebiej
     for f in os.listdir(way):
         path = way+"/"+f
         if os.path.isfile(path): # jest to plik
