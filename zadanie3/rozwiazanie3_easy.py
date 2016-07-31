75a8f8a793791b5b6b9c9d55f11bedcf0180478f
Piotr Gdowski <piotrekgdowski@wp.pl>
2016-08-01 00:37:15 +0200

    nie działa touch, cd, info

--- zadanie3/rozwiazanie3_easy.py
+++ zadanie3/rozwiazanie3_easy.py
@@ -26,22 +26,22 @@ def info(path):
     if os.path.isfile(path):
         typ = 'plik'
         size = (str(os.path.getsize(path))).join('B')
-        return path, '\n'.join(['typ: '.join(typ), 'sciezka: '.join(way),
-                          'rozmiar: '.join(size), 
-                          'ctime '.join('-'.join([str(ct.year), str(ct.month), str(ct.day)])),
-                          'mtime '.join('-'.join([str(mt.year), str(mt.month), str(mt.day)]))])
+        return path, '\n'.join(['typ: '+typ, 'sciezka: '+(way),
+                          'rozmiar: '+(size), 
+                          'ctime '+('-'.join([str(ct.year), str(ct.month), str(ct.day)])),
+                          'mtime '+('-'.join([str(mt.year), str(mt.month), str(mt.day)]))])
     if os.path.isdir(path):
         typ = 'katalog'
         size, count = get_size(path)
-        return path, '\n'.join(['typ: '.join(typ), 'sciezka: '.join(way),
-                          'rozmiar: '.join(size), 'liczba_plikow: '.join(count),
-                          'ctime '.join('-'.join([str(ct.year), str(ct.month), str(ct.day)])),
-                          'mtime '.join('-'.join([str(mt.year), str(mt.month), str(mt.day)]))])
+        return path, '\n'.join(['typ: '+(typ), 'sciezka: '+(way),
+                          'rozmiar: '+(size), 'liczba_plikow: '+(count),
+                          'ctime '+('-'.join([str(ct.year), str(ct.month), str(ct.day)])),
+                          'mtime '+('-'.join([str(mt.year), str(mt.month), str(mt.day)]))])
     
-    return path, '\n'.join(['typ: '.join(typ), 'sciezka: '.join(way),
-                          'rozmiar: '.join(size), 
-                          'ctime '.join('-'.join([str(ct.year), str(ct.month), str(ct.day)])),
-                          'mtime '.join('-'.join([str(mt.year), str(mt.month), str(mt.day)]))])
+    return path, '\n'.join(['typ: '+(typ), 'sciezka: '+(way),
+                          'rozmiar: '+(size), 
+                          'ctime '+('-'.join([str(ct.year), str(ct.month), str(ct.day)])),
+                          'mtime '+('-'.join([str(mt.year), str(mt.month), str(mt.day)]))])
 def parse(cmd, *args, **kwargs):
     path = kwargs['oldpath']
     if cmd == 'pwd':
@@ -72,36 +72,3 @@ while True:
     path, output = parse(raw_input(), oldpath=path)
     if not output == None:
         print output
-
-def g(**kwargs):
-    if kwargs is not None:
-        for key, value in kwargs.iteritems():
-            key = 'tert'
-            print key
- 
-
-"""def line(key, tup, count, extlen= 5, sizelen= 15, histlen=50):
-    size, many = tup
-    return "{}{}{}{}\n".format(key.rjust(extlen),(str(size)+'B').rjust(sizelen),10*' ',(int(round(histlen*many/count))*"#").rjust(histlen))
-def info(way, data): 
-    for f in os.listdir(way):
-        path = os.path.join(way,f)
-        if os.path.isfile(path):
-            ext = path.split('.')[-1]
-            if ext in data:
-                data[ext][0]+=os.path.getsize(path)
-                data[ext][1]+=1
-            else:
-                data[ext]=[os.path.getsize(path),1]
-        else:
-            data = info(path,data)
-    return data
-output = {}
-way = sys.argv[1]
-output = info(way, output)
-count = 0
-for key in output:
-    count+=output[key][1]
-with open('dane.txt', 'w') as f:
-    for key in output:
-        f.write(line(key, output[key],count))"""
