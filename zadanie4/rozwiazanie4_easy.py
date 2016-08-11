916aae30b8c2a6f5456c24109c0c02b93f8b8c77
Piotr Gdowski <piotrekgdowski@wp.pl>
2016-08-11 20:40:30 +0200

    Add files via upload

--- zadanie4/rozwiazanie4_easy.py
+++ zadanie4/rozwiazanie4_easy.py
@@ -1,10 +1,31 @@
 #wyzwaniepython ZADANIE 4 (EASY)
 #16-08-2016
 #autor: @Dewastators
-import urllib                                       
+import Queue
+import threading
+
+# called by each thread
+def read(q, _):
+    txt = raw_input()
+    new = "t"
+    while not new=="":
+        new = raw_input()
+        txt+=("\n"+new)
+    print txt[:-1]
+    q.put(txt[:-1])
+def parse(txt, _):
+    print "454"
+                                     
   
 if __name__ == "__main__":
-    sock = urllib.urlopen("https://pl.wikipedia.org/wiki/Markdown") 
-    htmlSource = sock.read()                            
-    sock.close()                                        
-    print htmlSource
+    print "Podwojny enter konczy wprowadzanie tekstu do przerobienia"
+    
+    q = Queue.Queue()
+    taketxt = threading.Thread(target=read, args = (q, 0))
+    taketxt.daemon = True
+    taketxt.start()
+    parseit = threading.Thread(target=parse, args = (q.get(), 0))
+    parseit.daemon = True
+    parseit.start()
+    
+    
